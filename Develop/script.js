eaa547c06a0cb61926788b58cc8c3602b5fe03c2
Zurisha <andreamwest7@gmail.com>
2022-04-09 22:57:43 -0500

    added new criteria code and new createPasswprd function

--- Develop/script.js
+++ Develop/script.js
@@ -5,19 +5,64 @@
 //Welcome
 
 
-  alert("Welcome to password Generator! Please click ok to begin creating your password!")
-  
  // ask criteria questions
 
 
- var passLength = prompt("How many characters would you like in your password? Please choose a number between 8 and 128.");
- 
- var passUpperCase = prompt("Would you like uppercase letters? Please type 'yes' or 'no'.");  
-  
- var PassLowerCase = prompt("Would you like lowercase letters? Please type 'yes or 'no'.");
+ var  passLength = prompt("How many characters would you like to include in your password? Please choose a number between 8 and 128.");
+        // make sure numbers are between 8 and 128
+        if (passLength <= 8|| passLength <= 128)  {
+          alert("Password must be between 8 and 128. Please choose again");
+          var passLength = ("How many characters would you like to include in your password? Please choose a number between 8 and 128.");
+        } 
+ //make sure one letter criteria is met
+ if (passUpperCase === true || passLowerCase === true){
+ }
+  else {
+    alert("Password must have either lower case or upper case letters!")
+    return passLength;
+ };
 
- var passSpecial = prompt("Would you like special characters? Please type 'yes' or 'no.");
 
- var passNumeral= prompt("Would you like numbers? Please type 'yes' or 'no'?");
-    
+  // string for criteria
+
+  upper  = "ABCDEFGHIJKLMNOPQRSTUVWXWYZ";
+  lower = "abcdefghijklmnopqrstuvwxyz";
+  special =  "!#$%&'()*+,-./:;<=>?@[^_{|}~";
+  numerals = "1234567890";
   
+  var passUpperCase = confirm("Would you like to include uppercase letters? Please type 'yes' or 'no'.");  
+  
+ var passLowerCase = confirm("Would you like to include lowercase letters? Please type 'yes or 'no'.");
+
+ var passSpecial = confirm("Would you like  to include special characters? Please type 'yes' or 'no.");
+
+ var passNumbers = confirm("Would you like to include numbers? ");
+
+
+
+  
+   var createPassword = function(characters){
+     var word = "";
+     criteria =
+
+     for(var i = 0; i < characters; i++)
+      word += criteria.charAt(Math.floor(Math.random() * criteria.length));
+console.log (createPassword);
+   };
+
+
+
+
+
+  
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+   addEventListener(click)
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+};
+createPassword();
