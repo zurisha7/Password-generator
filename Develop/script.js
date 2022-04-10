e5c223e405b3619add2d1318a427f87ec1672095
Zurisha <andreamwest7@gmail.com>
2022-04-10 00:28:48 -0500

    finished features working on create Password

--- Develop/script.js
+++ Develop/script.js
@@ -1,47 +1,108 @@
-//Password Generator JS
-
-/*Functions*/
-
-//Welcome
-
-
- // ask criteria questions
-
-
- var  passLength = prompt("How many characters would you like to include in your password? Please choose a number between 8 and 128.");
-        // make sure numbers are between 8 and 128
-        if (passLength <= 8|| passLength <= 128)  {
+//funtion to create click to start
+var generateBtn = document.querySelector("#generate");
+    generateBtn.addEventListener("click", function(){
+      word = createPassword();
+      document.getElementById("password")
+    })
+// functions to create password
+createPassword= function(){
+  passLength = parseInt(prompt("How many characters would you like to include in your password? Please choose a number between 8 and 128."));
+        // make sure number is between 8 and 128
+        if (passLength < 8|| passLength < 128)  {
           alert("Password must be between 8 and 128. Please choose again");
-          var passLength = ("How many characters would you like to include in your password? Please choose a number between 8 and 128.");
-        } 
+        }//make sure there is a number given
+        else if
+          (!passLength) {
+            alert("Please provide a number!")
+        }
+        //conirm criteria questions
+        else{
+        passUpper = confirm("Would you like to include uppercase letters? Please type 'yes' or 'no'.");  
+  
+        passLower = confirm("Would you like to include lowercase letters? Please type 'yes or 'no'.");
+
+        passSpecial = confirm("Would you like  to include special characters? Please type 'yes' or 'no.");
+
+        passNumbers = confirm("Would you like to include numbers? ");
+        };
+
+
  //make sure one letter criteria is met
- if (passUpperCase === true || passLowerCase === true){
+ if (passUpperCase !== true || passLowerCase !== true){
+    alert("Password must have either lower case or upper case letters!");
  }
-  else {
-    alert("Password must have either lower case or upper case letters!")
-    return passLength;
- };
+ //make sure at least one criteria is met
+ else if (!passUpper && !passLower && !passSpecial && !passNumbers) {
+        alert("Password must have at least one criteria!!");
+      }
+  //what to do with one criteria confirmed
+  else if (passUpper) {
+    criteria = upper;
+  }
+  else if (passLower) {
+    criteria = lower;
+  }
+  else if (passSpecial){
+    criteria = special;
+  }
+  else if (passNumbers){
+    criteria = numbers;
+  }
+  // two confirmed criteria
+  else if(passUpper && passLower) {
+    criteria = upper.concat(lower);
+  }
+  else if(passUpper && passSpecial){
+    criteria = upper.concat(special);
+  }
+  else if(passUpper && passNumbers){
+    criteria = upper.concat(numbers);
+  }
+  else if(passLower && passSpecial){
+    criteria = lower.concat(special);
+  }
+  else if(passLower && passNumber){
+    criteria = lower.concat(numbers);
+  }
+  else if(passSpecial && passNumbers){
+    criteria = special.concat(numbers);
+  }
+  // three criteria confirmed
+  else if(passUpper && passLower && passNumbers){
+    criteria = upper.concat(lower, numbers);
+  }
+  else if(passUpper && passLower && passSpecial){
+    criteria = upper.concat(lower, specials);
+  }
+  else if(passUpper && passNumbers && passSpecial){
+    criteria = upper.concat(numbers, special);
+  }
+  else if(passLower && passNumbers && passSpecial){
+    criteria = lower.concat(numbers,specials);
+  }
+   //4 confirmed criteria
+  else if (passUpper && passLower && passSpecial && passNumbers){
+    criteria = upper.concat(lower, numbers, special);
+  }
 
-
-  // string for criteria
+  // string for criteria 
 
   upper  = "ABCDEFGHIJKLMNOPQRSTUVWXWYZ";
   lower = "abcdefghijklmnopqrstuvwxyz";
   special =  "!#$%&'()*+,-./:;<=>?@[^_{|}~";
   numerals = "1234567890";
   
-  var passUpperCase = confirm("Would you like to include uppercase letters? Please type 'yes' or 'no'.");  
+  //Variables
+  var passLength;
+  var passLower;
+  var passUpper;
+  var passSpecial;
+  var passNumbers;
+  var criteria;
+
+
   
- var passLowerCase = confirm("Would you like to include lowercase letters? Please type 'yes or 'no'.");
-
- var passSpecial = confirm("Would you like  to include special characters? Please type 'yes' or 'no.");
-
- var passNumbers = confirm("Would you like to include numbers? ");
-
-
-
-  
-   var createPassword = function(characters){
+   var createPassword = function(){
      var word = "";
      criteria =
 
@@ -57,12 +118,9 @@ console.log (createPassword);
   
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-   addEventListener(click)
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-};
-createPassword();
